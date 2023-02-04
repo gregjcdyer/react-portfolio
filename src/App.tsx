4ea1557f3306472f341784e59ae06683bfa5346a
Greg Dyer <gregjcdyer@gmail.com>
2023-02-03 20:35:33 -0800

    added router, start creating layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './layout/NavBar';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <>
+          <NavBar />
+        </>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />}>
+              <Route index element={<h1>Home</h1>} />
+              <Route path='*' element={<h1>Not Found</h1>} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/layout/NavBar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
