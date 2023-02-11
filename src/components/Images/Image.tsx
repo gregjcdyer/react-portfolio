90096989a011fc6810e75d02e849abfcf4f8f3ba
Greg Dyer <gregjcdyer@gmail.com>
2023-02-10 20:22:35 -0800

    feat: hard code images for testing

--- src/components/Images/Image.tsx
+++ src/components/Images/Image.tsx
@@ -6,7 +6,15 @@ interface Props {
 }
 
 const Image: React.FC<Props> = ({ src, alt }) => {
-  return <img src={src} alt={alt} />;
+  return <img style={styles.image} src={src} alt={alt} />;
+};
+
+const styles = {
+  image: {
+    width: '200px',
+    height: 'auto',
+    padding: '1rem',
+  },
 };
 
 export default Image;
