f21ac945f1b980564632ef08c45b230bd853d919
Greg Dyer <gregjcdyer@gmail.com>
2024-10-01 20:34:14 -0700

    feat: full screen modal on click

--- src/components/Images/ImageGallery.tsx
+++ src/components/Images/ImageGallery.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import { ImageList, ImageListItem } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  ImageList,
+  ImageListItem,
+  Dialog,
+  DialogContent,
+  IconButton,
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 export interface ImageGalleryProps {
   images: { src: string; alt: string; id: string }[];
 }
 
 const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
+  const [selectedImage, setSelectedImage] = useState<{
+    src: string;
+    alt: string;
+  } | null>(null);
+
+  const handleImageClick = (img: { src: string; alt: string }) => {
+    setSelectedImage(img);
+  };
+
+  const handleClose = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div>
       <ImageList variant='masonry' cols={4} gap={8}>
         {images.map((img) => (
-          <ImageListItem key={img.id}>
+          <ImageListItem key={img.id} onClick={() => handleImageClick(img)}>
             <img
               src={img.src}
-              alt=''
-              style={{ width: '100%', height: 'auto' }}
+              alt={img.alt}
+              style={{ width: '100%', height: 'auto', cursor: 'pointer' }}
             />
           </ImageListItem>
         ))}
       </ImageList>
+
+      <Dialog
+        open={!!selectedImage}
+        onClose={handleClose}
+        maxWidth='md'
+        fullWidth
+      >
+        <DialogContent style={{ position: 'relative' }}>
+          <IconButton
+            style={{ position: 'absolute', top: 10, right: 10 }}
+            onClick={handleClose}
+          >
+            <CloseIcon />
+          </IconButton>
+          {selectedImage && (
+            <img
+              src={selectedImage.src}
+              alt={selectedImage.alt}
+              style={{ width: '100%', height: 'auto' }}
+            />
+          )}
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
